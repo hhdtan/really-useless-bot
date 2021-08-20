cb0c3fadc2ccf6937d17530fca40563c377861e6
hhdtan <thh.daric@gmail.com>
2021-08-21 02:04:02 +0800

    basic bot skeleton
    bare minimum required the bot, type "node ." to initialise bot

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+
+
+const bot = new Discord.Client({ intents: ["GUILDS","GUILD_MEMBERS" ,"GUILD_MESSAGES"] });
 
 const token = 'ODc4Mjc3MDMwMzU2MDgyNjk4.YR-1OQ.2SNIimxyNMD--kFd51HyY9tnk0I';
 

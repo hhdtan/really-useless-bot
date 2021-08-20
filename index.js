ff23a9f45e716abc132afb29c98048a41249f3ed
hhdtan <thh.daric@gmail.com>
2021-08-21 00:34:21 +0800

    f

---
+++ index.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const token = 'ODc4Mjc3MDMwMzU2MDgyNjk4.YR-1OQ.2SNIimxyNMD--kFd51HyY9tnk0I';
+
+bot.on('ready', ()=> {
+    console.log('this bot is online');
+});
+
+bot.login(token);

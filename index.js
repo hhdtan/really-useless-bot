254fcc4cab1e980f8abfa1377b1baebe51f7eb5a
hhdtan <thh.daric@gmail.com>
2021-08-25 18:19:29 +0800

    .env concealling token test

--- index.js
+++ index.js
@@ -3,16 +3,28 @@ const Discord = require('discord.js');
 
 const bot = new Discord.Client({ intents: ["GUILDS","GUILD_MEMBERS" ,"GUILD_MESSAGES"] });
 
-const token = 'ODc4Mjc3MDMwMzU2MDgyNjk4.YR-1OQ.2SNIimxyNMD--kFd51HyY9tnk0I';
+const TOKEN = 'ODc4Mjc3MDMwMzU2MDgyNjk4.YR-1OQ.2SNIimxyNMD--kFd51HyY9tnk0I';
+
+const PREFIX = '!'; // bot command prefix
 
 bot.on('ready', ()=> {
-    console.log('online');
+    console.log('bot is online');
 });
 
-bot.on('messageCreate',msg=>{
-    if(msg.content === "hi"){
-        msg.reply("the useless bot says hi");
+bot.on('messageCreate', msg=>{
+    
+    let args = msg.content.substr(PREFIX.length).split(" ");
+
+    switch(args[0]){
+        case 'ping':
+            msg.channel.send('pong'); // sends message to channel
+            //msg.reply('pong'); uses discord reply function
+            break;
+        case Number:
+            
+            
     }
+
 });
 
-bot.login(token);
+bot.login(TOKEN);

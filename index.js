fb32e7d341b4977b8dd4e93a3b3db016a0e92975
hhdtan <thh.daric@gmail.com>
2021-08-25 21:50:21 +0800

    CRON, token, catculate
    CRON --> event scheduling works
    token is now hidden
    catculate is in progress, somewhat working

--- index.js
+++ index.js
@@ -1,30 +1,68 @@
 const Discord = require('discord.js');
-
+const cron = require('cron');
 
 const bot = new Discord.Client({ intents: ["GUILDS","GUILD_MEMBERS" ,"GUILD_MESSAGES"] });
 
-const TOKEN = 'ODc4Mjc3MDMwMzU2MDgyNjk4.YR-1OQ.2SNIimxyNMD--kFd51HyY9tnk0I';
+const { TOKEN } = require('./config.json');
 
 const PREFIX = '!'; // bot command prefix
 
+var cat = 50; 
+
 bot.on('ready', ()=> {
     console.log('bot is online');
+    console.log(`cat has ${cat} grams left`);
+    
+    let scheduledMessage = new cron.CronJob('49 21 * * *', () => {
+        // Specifing your guild (server) and your channel
+        bot.channels.fetch('878278144497418272').then(channel => {
+            channel.send("CRON IS WORKING BITCHES");
+        })
+    });
+    scheduledMessage.start();
 });
 
 bot.on('messageCreate', msg=>{
-    
+    in_prefix = msg.content.substr(0,PREFIX.length);
     let args = msg.content.substr(PREFIX.length).split(" ");
 
-    switch(args[0]){
-        case 'ping':
-            msg.channel.send('pong'); // sends message to channel
-            //msg.reply('pong'); uses discord reply function
-            break;
-        case Number:
-            
+    // first switch statement checks for input prefix '!'
+    switch(in_prefix){
+        case '!':
+            switch(args[0]){
+                case 'ping':
+                    msg.channel.send('pong'); // sends message to channel
+                    //msg.reply('pong'); uses discord reply function
+                    break;
+                case 'feed':
+                    catculate(args[1],msg);
+                    break
+            // 
+        }
+        
+        // case '@':
+        //     switch(args[0]){
+        //         case 'on':
+        //             daily_reset.start();
+        //         case 'off':
+        //             daily_reset.stop();
+        //     }
+
             
     }
 
 });
 
 bot.login(TOKEN);
+
+function catculate(food, msg){
+    cat -= food;
+    console.log(`cat has ${cat} grams left`);
+    msg.channel.send(`cat has ${cat} grams left`);
+}
+
+function daily_reset(msg){
+
+    console.log('reset cat works');
+    cat = 0;
+}
